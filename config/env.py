5859bfb22874c0cb982db99c90581628370c05f3
AutumnChill <huxiajie@outlook.com>
2023-06-19 12:59:01 +0800

    0619

--- config/env.py
+++ config/env.py
@@ -6,12 +6,12 @@ from utils.load import load_ini
 
 CONFIG_PTAH = os.path.split(os.path.realpath(__file__))[0]
 # db_config.ini路径
-DB_ENV_PATH = os.path.join(CONFIG_PTAH, "db_config.ini")
+DB_CONFIG_PATH = os.path.join(CONFIG_PTAH, "db_config.ini")
 # sql.yml路径
-SQL_TASK_PATH = os.path.join(CONFIG_PTAH, "sql.yml")
+SQL_PATH = os.path.join(CONFIG_PTAH, "sql.yml")
 
 # 测试环境数据库连接URI
-db_config = load_ini(DB_ENV_PATH)
+db_config = load_ini(DB_CONFIG_PATH)
 DB_URI_TEST = "mysql+pymysql://%s:%s@%s:%s/%s?charset=utf8" % (db_config["test"]["user"], db_config["test"]["pwd"],
                                                                db_config["test"]["host"], db_config["test"]["port"],
                                                                db_config["test"]["database"])
--- utils/database.py
+++ utils/database.py
@@ -1,1 +1,24 @@
 # -*- coding: utf-8 -*-
+import pymysql
+
+from config.env import DB_CONFIG_PATH
+from utils.load import load_ini
+
+
+class MySqlBaseOPeration(object):
+    """pymysql 原生SQL操作"""
+
+    def __init__(self, env):
+        self.conn = None
+        self.env = env
+        self.db_config = load_ini(DB_CONFIG_PATH)
+
+    def connect(self):
+        self.conn = pymysql.connect(host=self.db_config[self.env]["host"],
+                                    port=self.db_config[self.env]["port"],
+                                    user=self.db_config[self.env]["user"],
+                                    password=self.db_config[self.env]["pwd"],
+                                    database=self.db_config[self.env]["database"],
+                                    charset="utf8")
+    def close(self):
+        self.conn.close()
